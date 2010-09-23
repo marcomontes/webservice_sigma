9b64d1ec6a7e8a0feaccb32b6eaca8b4bae3521e
Marco Montes <marco@time4code.com>
2010-09-22 22:51:22 -0500

    Add latest changes

--- app/controllers/wsaud_controller.rb
+++ app/controllers/wsaud_controller.rb
@@ -1,60 +1,78 @@
 class WsaudController < ApplicationController
 
-  protect_from_forgery :except => :respuesta_inscripcion_audiencia
-  #skip_before_filter :verify_authenticity_token
-
-
-   # Recibe la información de una Autoridad Ambiental para la celebración de una Audiencia Pública
+  # Recibe la información de una Autoridad Ambiental para la celebración de una Audiencia Pública
   def recibir_datos_audiencia_publica
-    #  Número Silpa de la Solicitud de Audiencia Pública
-    numero_silpa = params[:numero_silpa]
-    #  Fecha de Expedición del Edicto
-    #  Identificación del Solicitante
-    #  Lista con la Identificación de las entidades y de la comunidad del municipio donde se pretende desarrollar la audiencia pública ambiental
-    #  Nombre del proyecto, Obra o Actividad
-    #  Número de expediente
-    #  Identificación de la persona natural o jurídica interesada en la licencia o permiso ambiental.
-    #  Fecha de celebración de la audiencia pública.
-    #  Lugar de celebración de la audiencia pública.
-    #  Lista de Lugar(es) donde se podrá realizar la inscripción de ponentes.
-    #  Lista de Lugar(es) donde estarán disponibles los estudios ambientales para ser consultados.
-    #  Fecha de realización de la reunión informativa
-    #  Lugar de realización de la reunión informativa
-    #  Convocatoria a quienes deseen asistir y/o intervenir como ponentes.
-    #  Edicto Adjunto
-    #  Id de la Autoridad Ambiental
-    
-  end
+    response = create_response
+    soap_body = VitalAdapter.soap_body('wsaud', 'recibir_datos_audiencia_publica') 
 
-  def respuesta_inscripcion_audiencia
-    id_audiencia_publica = params[:id_audiencia_publica]
-    #ria = VitalAdapter.respuesta_inscripcion_audiencia(id_audiencia_publica)
-    #@ria = {
-#      :id_inscrito => ria.IdInscrito,
-#      :nombre_inscrito => ria.NombreInscrito,
-#      :cedula_inscrito => ria.CedulaInscrito,
-#      :lugar_expedicion => ria.LugarExpedicion,
-#      :correo_inscrito => ria.CorreoInscrito,
-#      :aprobado_inscrito => ria.AprobadoInscrito,
-#      :motivo => ria.Motivo
-#    }
-    @ria = {:metodo => "Respuesta Inscripcion Audiencia"}
+    response.recibir_datos_audiencia_publica{
+      |soap| soap.body = "{ #{soap_body} }"
+    }
 
     respond_to do |format|
-      format.xml  { render :xml => @ria }
+      format.xml  { render :xml => response }
+    end
+  end
+
+  # Recibe la información de una Autoridad Ambiental con la respuesta a la inscripción de Audiencia Pública
+  def respuesta_inscripcion_audiencia
+    response = create_response
+    soap_body = VitalAdapter.soap_body('wsaud', 'respuesta_inscripcion_audiencia')
+
+    response.respuesta_inscripcion_audiencia{
+      |soap| soap.body = "{ #{soap_body} }"
+    }
+
+    respond_to do |format|
+      format.xml  { render :xml => response }
     end
   end
 
   def aprueba_inscripcion_audiencia
+    response = create_response
+    soap_body = VitalAdapter.soap_body('wsaud', 'aprueba_inscripcion_audiencia')
 
+    response.aprueba_inscripcion_audiencia{
+      |soap| soap.body = "{ #{soap_body} }"
+    }
+
+    respond_to do |format|
+      format.xml  { render :xml => response }
+    end
   end
 
+  # SILPA expone los datos de inscripción para intervenir en una audiencia pública
   def datos_inscripcion_audiencia
-    
+    response = create_response
+    soap_body = VitalAdapter.soap_body('wsaud', 'datos_inscripcion_audiencia')
+
+    response.datos_inscripcion_audiencia{
+      |soap| soap.body = "{ #{soap_body} }"
+    }
+
+    respond_to do |format|
+      format.xml  { render :xml => response }
+    end
   end
 
+  # Recibe la información de inscripción para intervenir en una audiencia pública
   def inscripcion_audiencia_aa
-    
+    response = create_response
+    soap_body = VitalAdapter.soap_body('wsaud', 'inscripcion_audiencia_aa')
+
+    response.recibir_datos_audiencia_publica{
+      |soap| soap.body = "{ #{soap_body} }"
+    }
+
+    respond_to do |format|
+      format.xml  { render :xml => response }
+    end
   end
+
+  protected
+
+    def create_response
+      VitalAdapter.new_client('WSAUD')
+    end
 
 end
--- app/models/vital_adapter.rb
+++ app/models/vital_adapter.rb
@@ -1,6 +1,8 @@
 class VitalAdapter
   
   WS_CONFIG = YAML::load(ERB.new(File.read("#{RAILS_ROOT}/config/webservice.yml")).result)[RAILS_ENV]
+  VITAL_FIELDS = YAML::load(ERB.new(File.read("#{RAILS_ROOT}/config/vital.yml")).result)
+
   WSDL = WS_CONFIG['wsdl_root']
 
   def self.new_client(service)
@@ -10,6 +12,24 @@ class VitalAdapter
   def self.get_actions(service)
     client = new_client(service)
     client.wsdl.soap_actions
+  end
+
+#  def self.create_response(service, method)
+#    soap_body = soap_body("#{service}", "#{method}")
+#    response = new_client("#{service}")
+#    response.method{
+#      |soap| soap.body = "{ #{soap_body} }"
+#    }
+#  end
+
+  def self.soap_body(service, method)
+    fields = VITAL_FIELDS["#{service}"]["#{method}"]
+    soap_body = String.new
+    fields.each do |f|
+      soap_body << ":#{f} => params[:#{f}]"
+      soap_body << ", " if f != fields.last
+    end
+    soap_body
   end
 
 end
