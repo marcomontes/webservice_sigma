d175b57d0db05ec33fd79cdf5d589b5b0b681d31
Marco Montes <marco@time4code.com>
2010-09-27 00:19:37 -0500

    Corpocaldas select and insert methods

--- app/controllers/corpocaldas_controller.rb
+++ app/controllers/corpocaldas_controller.rb
@@ -1,44 +1,23 @@
 class CorpocaldasController < ApplicationController
 
-  def consultar
-    tabla = params[:tabla]
-    campos = params[:campos]
-    condicion = params[:condicion]
-    sql = "SELECT #{campos} FROM #{tabla} WHERE #{condicion}"
-  end
-
-  def insertar
-
-  end
-
-  def test
-    @test = Corpocaldas.find_by_sql("select * from test")
-    respond_to do |format|
-      format.xml  { render :xml => @test }
-    end
-  end
-
   def consulta
+    fields = params[:campos]
+    tables = params[:tablas]
+    conditions = params[:condiciones]
 
-    @consulta = Corpocaldas.find_by_sql("select usu_identi as Id_Persona, (usu_nombre||' '||validar_null(usu_priape)||' '||validar_null(usu_segape)) as Solicitante, usu_email as Correo_Electronico,
-usu_telefo as Telefono, usu_tipide as Tipo_Id, usu_lugexp as Lugar_Exped, usu_celular as Celular, usu_fax as Fax, usu_tarpro as Tarjeta_Pro,
-usu_direcco as Direccion, usu_depart as Id_Depto, dep_nombre as Nombre_Depto, usu_ciudad as Municipio, usu_vereda as Vereda
-from t_usuari, t_depart
-where t_usuari.usu_identi = '10141490' --ejemplo
-and t_usuari.usu_depart = t_depart.dep_codigo")
+    sql = "SELECT #{fields} FROM #{tables} WHERE #{conditions}"
+    sql_result = Corpocaldas.find_by_sql(sql)
 
-#    codigo = params[:codigo]
-#    @consulta = Usuario.find_by_sql("select per_codigo as Id_Tramite, per_tipo as Tipo_Proceso, t_permiso.usu_identi as Id_Solicitante,
-#(usu_nombre||' '||validar_null(usu_priape)||' '||validar_null(usu_segape)) as Solicitante
-#from t_permiso, t_usuari
-#where t_permiso.per_codigo = '#{codigo}' --ejemplo
-#and t_permiso.usu_identi = t_usuari.usu_identi#")
+    output = VitalAdapter.output_type
 
-    respond_to do |format|
-
-      format.xml  { render :xml => @consulta }
+    if output == 'download'
+      file_name = "corpocaldas_query.xml"
+      VitalAdapter.generate_file(file_name, sql_result)
+      send_file "#{RAILS_ROOT}/tmp/#{file_name}" , :filename => "#{file_name}"
+    elsif output == 'xml'
+      respond_to do |format| format.xml  { render :xml => sql_result } end
     end
 
   end
-  
+
 end
--- app/models/intermedia.rb
+++ app/models/intermedia.rb
@@ -1,3 +1,59 @@
 class Intermedia < ActiveRecord::Base
   set_table_name "t_intermedia"
+  set_primary_key :int_codigo
+
+  def self.create_record(response)
+    record = self.new
+
+    record.int_codigo = response.codigo
+    record.int_tipo = nil
+    record.int_estado = nil
+    record.int_fecreg = nil
+    record.int_tipsol = nil
+    record.int_nomraz = nil
+    record.int_priape = nil
+    record.int_segape = nil
+    record.int_tipide = nil
+    record.int_idesol = nil
+    record.int_lugexp = nil
+    record.int_direco = nil
+    record.int_codeso = nil
+    record.int_comuso = nil
+    record.int_telefo = nil
+    record.int_fax = nil
+    record.int_email = nil
+    record.int_nomrep = nil
+    record.int_tidere = nil
+    record.int_iderep = nil
+    record.int_dircor = nil
+    record.int_codere = nil
+    record.int_comure = nil
+    record.int_telere = nil
+    record.int_faxre = nil
+    record.int_emailr = nil
+    record.int_nomapo = nil
+    record.int_tideap = nil
+    record.int_ideapo = nil
+    record.int_dircoa = nil
+    record.int_codeap = nil
+    record.int_comuap = nil
+    record.int_teleap = nil
+    record.int_faxapo = nil
+    record.int_emaila = nil
+    record.int_tarpro = nil
+    record.int_nompro = nil
+    record.int_valpro = nil
+    record.int_deppro = nil
+    record.int_munpro = nil
+    record.int_verpro = nil
+    record.int_sector = nil
+    record.int_ficcat = nil
+    record.int_activi = nil
+    record.int_calact = nil
+    record.int_numrad = nil
+    record.int_fecrad = nil
+    record.per_codigo = nil
+
+    record.save
+  end
 end
