1a722e2eb4e3c193b172463b000ec3725e69708e
Marco Montes <marco@time4code.com>
2010-10-01 18:06:13 -0500

    add untracked file

---
+++ app/models/consultas.rb
@@ -0,0 +1,14 @@
+class Corpocaldas < ActiveRecord::BaseWithoutTable
+
+  def self.query(sql)
+    sql_result = Corpocaldas.find_by_sql(sql)
+    sql_result
+  end
+
+  def self.custom_query(fields, tables, conditions)
+    sql_query = "SELECT #{fields} FROM #{tables} WHERE #{conditions}"
+    sql_result = Corpocaldas.find_by_sql(sql_query)
+    sql_result
+  end
+  
+end
